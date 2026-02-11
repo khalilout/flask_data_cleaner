de34ecd871a6f492412d80b364c5178746d8a7b6
khalilout <khalilouybt@gmail.com>
2026-02-11 12:12:24 +0000

    12éme commit

--- app.py
+++ app.py
@@ -312,5 +312,6 @@ def import_file():
     return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
     # app.run(debug=True, port=5000)
